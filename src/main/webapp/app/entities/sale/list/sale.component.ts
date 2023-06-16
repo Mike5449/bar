9a9a74425317b3489850f6ae1f874439ef321c4e
Mike5449 <jeanlouismike89@gmail.com>
2023-06-16 14:54:27 -0400

    other version

--- src/main/webapp/app/entities/sale/list/sale.component.ts
+++ src/main/webapp/app/entities/sale/list/sale.component.ts
@@ -25,6 +25,11 @@ export enum TypeAction {
   UPDATE,
   DETAIL,
 }
+
+// round(value: number, decimals: number): number {
+//   const factor = Math.pow(10, decimals);
+//   return Math.round(value * factor) / factor;
+// }
 @Component({
   selector: 'jhi-sale',
   templateUrl: './sale.component.html',
